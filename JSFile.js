dd21ed09bbea6e8421196f212095553dbc9d3d03
Rahma090 <rahma.aliani@gmail.com>
2023-10-09 13:48:52 +0100

    update

---
+++ JSFile.js
@@ -0,0 +1,14 @@
+function clickButton1() {
+var button = document.getElementById('level1')
+beginner.src = $(<a href=""></a>)
+}
+
+function clickButton2() {
+var button = document.getElementById('level2')
+intermediate.src = $(<a href=""></a>)
+}
+
+function clickButton3() {
+var button = document.getElementById('level3')
+advanced.src = $(<a href=""></a>)
+}
